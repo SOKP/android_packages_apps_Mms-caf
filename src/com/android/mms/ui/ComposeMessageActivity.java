6e459f0bbfc3b0ff49a37b3c05ee1f5b6ac294e7
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-08-13 14:01:38 -0700

    Unbreak Mms
    Change-Id: I2d9ac569294ef689562c169fab940ee7cafcefc1

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -4467,7 +4467,7 @@ public class ComposeMessageActivity extends Activity
             }
             @Override
             public void onScrollStateChanged(AbsListView view, int scrollState) {
-                scrolling = scrollState != OnScrollListener.SCROLL_STATE_IDLE;
+                scrolling = scrollState != AbsListView.OnScrollListener.SCROLL_STATE_IDLE;
             }
         });
         mMsgListView.setMultiChoiceModeListener(new ModeCallback());
